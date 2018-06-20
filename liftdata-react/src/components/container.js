84ca67f6bcf73e554ffe0e52481bb6ba5e721779
pattruong <pattruong@gmail.com>
2018-06-19 20:40:56 -0700

    basic frontend built

--- liftdata-react/src/components/container.js
+++ liftdata-react/src/components/container.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import InputForm from './input';
 
 class Container extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
+    state = {
+        entries: []
     }
-    render() { 
+
+    onSubmit = entry => {
+        this.setState(prevState => ({
+            entries: [...prevState.entries, entry]
+        }));
+    }
+
+    render() {
         return (
             <div>
-                Hello World!
+
+                {
+                    this.state.entries.map(({ exercise, weight, unit, sets, reps }, index) => (
+                        <ul key={index}>
+                            <li>{exercise}</li>
+                            <li>{`${weight} ${unit}`}</li>
+                            <li>{sets}</li>
+                            <li>{reps}</li>
+                        </ul>)
+                    )
+                }
+
+                <InputForm onSubmit={this.onSubmit} />
             </div>
         )
     }
 }
- 
+
 export default Container;
---
+++ liftdata-react/src/components/input.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+
+class InputForm extends Component {
+    state = {
+        exercise: '',
+        weight: '',
+        unit: 'lb',
+        sets: 1,
+        reps: 1
+    }
+
+    handleChange = ({ target }) => {
+        let { value, name, type } = target;
+        if (type === 'number' && value !== '') {
+            value = parseFloat(value);
+        }
+        this.setState({
+            [name]: value
+        });
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        // check for empty string weight 
+
+        // send up
+        this.props.onSubmit(this.state);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                    <label>Exercise</label>
+                    <input
+                        value={this.state.exercise}
+                        onChange={this.handleChange}
+                        name="exercise"
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter exercise name"
+                    />
+                    <small className="form-text text-muted">ex. squat, bench, or deadlift</small>
+                </div>
+                <div className="form-group">
+                    <label>Weight</label>
+                    <input
+                        value={this.state.weight}
+                        onChange={this.handleChange}
+                        name="weight"
+                        type="number"
+                        min="0"
+                        step="0.01"
+                        className="form-control"
+                        placeholder="Enter weight"
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Unit</label>
+                    <select
+                        value={this.state.unit}
+                        onChange={this.handleChange}
+                        name="unit"
+                        className="form-control"
+                    >
+                        <option value="lb">lb</option>
+                        <option value="kg">kg</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label>Sets</label>
+                    <input
+                        value={this.state.sets}
+                        onChange={this.handleChange}
+                        name="sets"
+                        type="number"
+                        min="1"
+                        step="1"
+                        className="form-control"
+                        placeholder="Enter sets"
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Reps</label>
+                    <input
+                        value={this.state.reps}
+                        onChange={this.handleChange}
+                        name="reps"
+                        type="number"
+                        min="1"
+                        step="1"
+                        className="form-control"
+                        placeholder="Enter reps"
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        )
+    }
+}
+
+export default InputForm;
